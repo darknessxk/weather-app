d99e9ba81e42dde49ea4af1c0d98f6b38f578b56
darknessxk <me@darknessxk.com>
2021-03-12 15:45:12 -0300

    Added tailwind back

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,15 @@
 import React, {  } from "react";
 import Layout from '../components/Layout';
-// import faker from 'faker';
+import faker from 'faker';
 
 const IndexPage = () => {
     return (
         <Layout title="Home">
             <main className="flex flex-row md:flex-col flex-grow h-full overflow-hidden subpixel-antialiased">
-
+                {faker.company.companyName()}
+                <hr />
+                {faker.company.catchPhrase()}
+                <br />
             </main>
         </Layout>
     );
