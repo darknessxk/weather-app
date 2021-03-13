b7d7ee05dc99b6fb3dbc116925276a1a14289051
darknessxk <me@darknessxk.com>
2021-03-12 22:21:08 -0300

    Changed master layout title format

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -12,7 +12,7 @@ const layout = ({children, title = '...', fluid = true}: Props) => {
 
     return <>
         <Head>
-            <title>{title} | darknessxk</title>
+            <title>{title}</title>
             <meta charSet="utf-8"/>
             <meta name="viewport" content="initial-scale=1.0, width=device-width"/>
             <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"/>
