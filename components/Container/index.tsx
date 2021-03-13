64b22bd17b5445d1dcb0494e585d3ed39c2ba6f8
darknessxk <me@darknessxk.com>
2021-03-13 17:21:54 -0300

    Changed some css attributes to existing tailwind classes

--- components/Container/index.tsx
+++ components/Container/index.tsx
@@ -2,12 +2,7 @@ import styled from 'styled-components';
 import tw from 'twin.macro';
 
 const Container = styled.div<{ fluid?: boolean }>`
-    ${tw`h-full w-full bg-gray-100`};
-    
-    flex: 1;
-    
-    display: flex;
-    flex-direction: column;
+    ${tw`h-full w-full bg-gray-100 flex flex-1 flex-col`};
     padding: 0 ${props => (props.fluid || false ? '0' : '50px')};
 `;
 
