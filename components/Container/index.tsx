0d8a0327e2f12a3c847f098484892d0980ec2def
darknessxk <me@darknessxk.com>
2021-03-13 18:01:00 -0300

    Centered item, added background

--- components/Container/index.tsx
+++ components/Container/index.tsx
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 import tw from 'twin.macro';
 
 const Container = styled.div<{ fluid?: boolean }>`
-    ${tw`h-full w-full bg-gray-100 flex flex-1 flex-col`};
+    ${tw`h-auto w-auto shadow-md bg-gray-200 flex flex-col`};
     padding: 0 ${props => (props.fluid || false ? '0' : '50px')};
 `;
 
