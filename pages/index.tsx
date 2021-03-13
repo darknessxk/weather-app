cfb8320c14b938e25085c1ffebe77d5fc5ecb501
darknessxk <me@darknessxk.com>
2021-03-13 18:01:24 -0300

    First page, request user information

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,26 @@
-import React, {  } from "react";
+import React, {useEffect, useState} from "react";
 import Layout from '../components/Layout';
-import faker from 'faker';
+import Text from "../components/Text";
 
 const IndexPage = () => {
+    const [location, setLocation] = useState<GeolocationPosition | undefined>(undefined);
+
+    useEffect(() => {
+        navigator.geolocation.getCurrentPosition(
+            (position) => {
+                setLocation(position);
+            },
+            () => console.error('not allowed'),
+            {enableHighAccuracy: true}
+        );
+    }, [setLocation]);
+
     return (
         <Layout title="Home">
             <main className="flex flex-row md:flex-col flex-grow h-full overflow-hidden subpixel-antialiased">
-                {faker.company.companyName()}
-                <hr />
-                {faker.company.catchPhrase()}
-                <br />
+                <Text>
+                    Waiting for user permission...
+                </Text>
             </main>
         </Layout>
     );
