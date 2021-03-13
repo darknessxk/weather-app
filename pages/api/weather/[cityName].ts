21383dc49d204a1079068e078de398c6f3ba8f7f
darknessxk <me@darknessxk.com>
2021-03-13 14:03:22 -0300

    Added cache to api call, lowering latency and api usage quota

--- pages/api/weather/[cityName].ts
+++ pages/api/weather/[cityName].ts
@@ -1,5 +1,11 @@
 import { NextApiRequest, NextApiResponse } from 'next'
 import axios from "axios";
+import NodeCache from 'node-cache'
+
+const cache = new NodeCache({
+    stdTTL: 20,
+    checkperiod: 120
+})
 
 export default async function weatherHandler(req: NextApiRequest, res: NextApiResponse) {
     const apiSecret = process.env.WEATHER_API;
@@ -13,18 +19,30 @@ export default async function weatherHandler(req: NextApiRequest, res: NextApiRe
         case 'GET':
             // Get data from your database
             try {
-                const response = await axios.request({
-                    method: 'GET',
-                    url: 'https://api.openweathermap.org/data/2.5/weather',
-                    params: {
-                        appid: apiSecret,
-                        q: cityName,
-                        units: 'metric',
-                        lang: langCode || 'en'
-                    }
-                });
+                const cacheKey = `${cityName}-${langCode}`;
+                let response: any;
+                if (cache.has(cacheKey)) {
+                    response = cache.get(cacheKey)
+                }
+                else
+                {
+                    const apiResponse = await axios.request({
+                        method: 'GET',
+                        url: 'https://api.openweathermap.org/data/2.5/weather',
+                        params: {
+                            appid: apiSecret,
+                            q: cityName,
+                            units: 'metric',
+                            lang: langCode || 'en'
+                        }
+                    });
 
-                res.status(200).json(response.data);
+                    const response = apiResponse.data;
+
+                    cache.set(cacheKey, response);
+                }
+
+                res.status(200).json(response);
             } catch (e) {
                 res.status(500).json(e);
             }
