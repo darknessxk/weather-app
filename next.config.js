c0f79db402273b0dd8b662a7423e2dcfff186b43
darknessxk <me@darknessxk.com>
2021-03-13 03:19:13 -0300

    Added proper env configuration

--- next.config.js
+++ next.config.js
@@ -4,17 +4,27 @@ console.log('Loading next configuration...');
 const compress = false;
 
 module.exports = (phase, { defaultConfig }) => {
+    const shared = {
+        serverRuntimeConfig: {
+            api: {
+                googleMaps: process.env.GOOGLE_MAPS_API,
+                weather: process.env.WEATHER_API
+            }
+        },
+        ...defaultConfig
+    }
+
     if (phase === PHASE_DEVELOPMENT_SERVER) {
         console.log('Development mode');
         return {
             compress,
-            ...defaultConfig
+            ...shared
         }
     }
 
     console.log('Production mode');
     return {
         compress,
-        ...defaultConfig
+        ...shared
     }
 }
