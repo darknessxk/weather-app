db3b7aae606e8dfb7e646f0cda3da12c1152f55f
darknessxk <me@darknessxk.com>
2021-03-14 13:48:34 -0300

    Added Language Reducer and Enum with available languages

--- redux/RootReducer.ts
+++ redux/RootReducer.ts
@@ -1,6 +1,8 @@
 import { combineReducers } from '@reduxjs/toolkit'
 import locationReducer from './reducers/location'
+import languageReducer from './reducers/language'
 
 export default combineReducers({
-    location: locationReducer
+    location: locationReducer,
+    language: languageReducer
 })
