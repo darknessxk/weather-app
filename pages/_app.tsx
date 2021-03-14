14e2b5bef899265a3c0ff8b453146a2d89a67513
darknessxk <me@darknessxk.com>
2021-03-14 18:58:44 -0300

    Code cleanup

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,18 +1,18 @@
 import React from 'react';
-import { AppProps } from 'next/app';
+import {AppProps} from 'next/app';
 import Head from 'next/head';
-import '../styles/index.css'
 import {Provider} from "react-redux";
+import '../styles/index.css'
 import {store} from "../redux/store";
 
 // NOTE: Router can be accessed through this App Context or any Page routed
 
-function AppMain({ Component, pageProps }: AppProps) {
+function AppMain({Component, pageProps}: AppProps) {
     return <Provider store={store}>
         <Head>
             <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no"/>
         </Head>
-        <Component children={pageProps} />
+        <Component children={pageProps}/>
     </Provider>
 }
 
