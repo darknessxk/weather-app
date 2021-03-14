54427213c6253e5c7d9b0da0e36422ed1bf2e873
darknessxk <me@darknessxk.com>
2021-03-14 18:56:45 -0300

    Code cleanup

--- redux/store.ts
+++ redux/store.ts
@@ -1,5 +1,4 @@
 import { configureStore } from '@reduxjs/toolkit'
-import { useDispatch } from 'react-redux'
 import rootReducer from './RootReducer'
 
 export const store = configureStore({
@@ -10,4 +9,3 @@ export const store = configureStore({
 
 export type RootState = ReturnType<typeof store.getState>
 export type AppDispatch = typeof store.dispatch
-export const useAppDispatch = () => useDispatch<AppDispatch>()
