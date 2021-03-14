259048879b1d2149e8f6963e2168fb31812f2448
darknessxk <me@darknessxk.com>
2021-03-14 13:17:31 -0300

    Added Manual and Automatic mode for fetching user location

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,26 +1,62 @@
-import React, {useEffect, useState} from "react";
+import React, {useCallback} from "react";
+import { setLocation } from "redux/reducers/location";
 import Layout from '../components/Layout';
 import Text from "../components/Text";
+import {useAppDispatch} from "../redux/store";
+import axios from "axios";
+import Tile from "../components/Tile";
 
 const IndexPage = () => {
-    const [location, setLocation] = useState<GeolocationPosition | undefined>(undefined);
+    const dispatch = useAppDispatch();
 
-    useEffect(() => {
+    const autoLocation = useCallback(() => {
         navigator.geolocation.getCurrentPosition(
             (position) => {
-                setLocation(position);
+                dispatch(setLocation({
+                    lat: position.coords.latitude,
+                    lon: position.coords.longitude
+                }))
             },
-            () => console.error('not allowed'),
+            async () => {
+                try {
+                    const apiResponse = await axios.request({
+                        url: 'https://api.my-ip.io/ip.json',
+                        method: 'GET'
+                    });
+
+                    const ipInfo = await axios.request({
+                        url: `http://ip-api.com/json/${apiResponse.data.ip}`,
+                        method: 'GET'
+                    });
+
+                    dispatch(setLocation({
+                        lat: ipInfo.data.lat,
+                        lon: ipInfo.data.lon
+                    }))
+                }
+                catch
+                {
+                    // now we go into form mode
+                }
+            },
             {enableHighAccuracy: true}
         );
-    }, [setLocation]);
+    }, []);
 
     return (
         <Layout title="Home">
             <main className="flex flex-row md:flex-col flex-grow h-full overflow-hidden subpixel-antialiased">
-                <Text>
-                    Waiting for user permission...
+                <Text className="text-center">
+                    Choose one option
                 </Text>
+                <div className="flex">
+                    <Tile>
+                        <Text onClick={() => autoLocation()}>Auto</Text>
+                    </Tile>
+                    <Tile>
+                        <Text>Manual</Text>
+                    </Tile>
+                </div>
             </main>
         </Layout>
     );
