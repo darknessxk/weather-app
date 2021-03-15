65a2272e57dd416278cb59ddc839dfd8d833bc59
darknessxk <me@darknessxk.com>
2021-03-15 13:34:35 -0300

    Added ready check, implemented location picker, and some location fallback methods

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,27 @@
-import React, {useCallback} from "react";
-import { setLocation } from "redux/reducers/location";
+import React, {useCallback, useState, useEffect} from "react";
+import { useRouter } from 'next/router'
+import {setLocation} from "redux/reducers/location";
 import Layout from '../components/Layout';
-import {useAppDispatch} from "../redux/hooks";
+import {useAppDispatch, useAppSelector} from "../redux/hooks";
 import Tile from "../components/Tile";
 import Text from "../components/Text";
 import fetchUserIp from "../utils/fetchUserIp";
 import fetchIpLocation from "../utils/fetchIpLocation";
+import LocationPicker from "../components/LocationPicker";
 
 const IndexPage = () => {
     const dispatch = useAppDispatch();
+    const location = useAppSelector(state => state.location);
+    const router = useRouter();
+    const [isReady, setIsReady] = useState<boolean>(false);
+
+    useEffect(() => {
+        if (isReady) {
+            (async () => {
+                await router.push(`/weather/${location.lat}/${location.lon}`);
+            })();
+        }
+    }, [isReady]);
 
     const autoLocation = useCallback(() => {
         navigator.geolocation.getCurrentPosition(
@@ -17,6 +30,8 @@ const IndexPage = () => {
                     lat: position.coords.latitude,
                     lon: position.coords.longitude
                 }))
+
+                setIsReady(true);
             },
             async () => {
                 try {
@@ -27,30 +42,42 @@ const IndexPage = () => {
                         lat: ipInfo.data.lat,
                         lon: ipInfo.data.lon
                     }))
-                }
-                catch
-                {
-                    // now we go into form mode
+
+                    setIsReady(true);
+                } catch {
+                    setShowLocationInput(true);
                 }
             },
             {enableHighAccuracy: true}
         );
     }, []);
 
+    const [showLocationInput, setShowLocationInput] = useState<boolean>(false)
+
     return (
         <Layout title="Home">
             <main className="flex flex-row md:flex-col flex-grow h-full overflow-hidden subpixel-antialiased">
-                <Text className="text-center">
-                    Choose how we get your location
-                </Text>
-                <div className="flex">
-                    <Tile>
-                        <Text onClick={() => autoLocation()}>Auto</Text>
-                    </Tile>
-                    <Tile>
-                        <Text>Manual</Text>
-                    </Tile>
-                </div>
+                {!showLocationInput ? (
+                        <>
+                            <Text className="text-center">
+                                Choose how we get your location
+                            </Text>
+                            <div className="flex">
+                                <Tile onClick={() => autoLocation()}>
+                                    <Text>Auto</Text>
+                                </Tile>
+                                <Tile onClick={() => setShowLocationInput(true)}>
+                                    <Text>Manual</Text>
+                                </Tile>
+                            </div>
+                        </>
+                    ) :
+                    (
+                        <div className="flex">
+                            <LocationPicker onReady={() => setIsReady(true)} />
+                        </div>
+                    )
+                }
             </main>
         </Layout>
     );
