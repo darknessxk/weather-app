24656696f177cb71c0a5431a7e4270500338a768
darknessxk <me@darknessxk.com>
2021-03-15 14:04:45 -0300

    Added and implemented WeatherItem into weather page

--- pages/weather/[...coord].tsx
+++ pages/weather/[...coord].tsx
@@ -1,25 +1,46 @@
-import React from "react";
+import React, {ReactNode, useEffect, useState} from "react";
 import Layout from '../../components/Layout';
-import Tile from "../../components/Tile";
 import Text from "../../components/Text";
 import OneCall from "../../api/open-weather/onecall";
 import {GetServerSidePropsContext, InferGetServerSidePropsType} from "next";
 import {OneCallResponse} from "../../api/open-weather/onecallresponse.type";
 import GeoDecoding from "../../api/google/geodecoding";
+import parseGeocodeName from "../../utils/parseGeocodeName";
+import WeatherItem from "../../components/WeatherItem";
 
 const WeatherPage = (props: InferGetServerSidePropsType<typeof getServerSideProps>) => {
-    console.log(props);
+    const {
+        children: {
+            data
+        }
+    } = props;
+
+    const location = data.location;
+    const weather: OneCallResponse = data.weather;
+
+    const [RenderItems, setItems] = useState<ReactNode>(null);
+    useEffect(() => {
+        let renderItems = [];
+
+        for (const [key, value] of Object.entries(weather.current)) {
+            if (key !== 'weather') {
+                renderItems.push(<WeatherItem name={key} value={value.toString()} />)
+            }
+        }
+
+        setItems(renderItems);
+    }, [weather]);
+
+    const locationName = parseGeocodeName(location.formatted_address);
 
     return (
-        <Layout title="Home">
+        <Layout title={`Weather for ${locationName}`}>
             <main className="flex flex-row md:flex-col flex-grow h-full overflow-hidden subpixel-antialiased">
                 <Text className="text-center">
-                    Testing....
+                    {locationName}
                 </Text>
-                <div className="flex">
-                    <Tile>
-                        2
-                    </Tile>
+                <div className="flex flex-col">
+                    {RenderItems}
                 </div>
             </main>
         </Layout>
